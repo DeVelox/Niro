f17335fba90ddd5ced8472bac36282f38d659ae2
RonShleif <rr.ron.rr@gmail.com>
2024-01-18 00:36:28 +0200

    fix slide and dash

--- player/player.gd
+++ player/player.gd
@@ -3,16 +3,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 const DASH_LENGTH = 0.2
-const DASH_MULTI = 2
-const SLIDE_LENGTH = 0.2
-const SLIDE_MULTI = 2
+const DASH_MULTI = 2.5
+const SLIDE_MULTI = 2.5
 const WALL_JUMP_MULTI = 2
 
 @onready var dash_timer = $DashTimer
 @onready var drop_check = $DropCheck
 @onready var drop_timer = $DropTimer
 @onready var coyote_timer = $CoyoteTimer
-@onready var slide_cooldown = $SlideCooldown
 @onready var sprite = $AnimatedSprite2D
 @onready var double_tap = $DoubleTap
 @onready var hitbox = $CollisionShape2D
@@ -27,7 +25,6 @@ var has_double_jump = false
 var is_dashing = false
 var has_dash = false
 var is_sliding = false
-var has_slide = true
 var platform
 var motion
 var was_on_floor = false
@@ -37,7 +34,9 @@ func _physics_process(delta):
 	# Reset character to start of level
 	if Input.is_action_just_pressed("reset"):
 		reload()
-	
+		
+	if Input.is_action_just_pressed("dash"):
+		try_dash()
 	# Do not allow other movement while dashing
 	if is_dashing:
 		move_and_slide()
@@ -46,7 +45,7 @@ func _physics_process(delta):
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
+
 	# Perform state checks
 	if is_on_floor():
 		has_double_jump = true
@@ -56,14 +55,11 @@ func _physics_process(delta):
 
 	# Establish baseline horizontal movement
 	motion = Input.get_axis("left", "right") * SPEED
-
+	
 	# Handle special movement.
 	if Input.is_action_just_pressed("jump"):
 		try_jump()
 
-	if Input.is_action_just_pressed("dash"):
-		try_dash()
-	
 	if Input.is_action_just_pressed("crouch"):
 		if double_tap.is_stopped():
 			double_tap.start()
@@ -90,7 +86,7 @@ func _physics_process(delta):
 	# Coyote timer
 	if is_on_floor():
 		was_on_floor = true
-
+	
 	move_and_slide()
 		
 	if was_on_floor and not is_on_floor():
@@ -99,6 +95,9 @@ func _physics_process(delta):
 
 func try_jump():
 	if is_on_floor() or not coyote_timer.is_stopped():
+		if is_sliding:
+			stop_slide()
+			velocity.x *= 1.3
 		velocity.y = JUMP_VELOCITY
 	elif is_wall_hanging:
 		# Apply a jump opposite of the wall hang
@@ -112,23 +111,26 @@ func try_jump():
 		return
 
 func try_dash():
-	if has_slide and is_on_floor():	
-		dash_timer.wait_time = SLIDE_LENGTH
+	if not is_sliding and is_on_floor():	
 		velocity.x = motion * SLIDE_MULTI
-		hitbox.position.y = 32
+		hitbox.position.y = 45
 		hitbox.scale.y = 0.3
-		slide_cooldown.start()
 		is_sliding = true
-		has_slide = false
 	elif has_dash and not is_on_floor():
 		dash_timer.wait_time = DASH_LENGTH
 		velocity.x = motion * DASH_MULTI
 		is_dashing = true
 		has_dash = false
+		dash_timer.start()
+		velocity.y = 0
 	else:
 		return
-	dash_timer.start()
-	velocity.y = 0
+
+
+func stop_slide():
+	hitbox.position.y = 0
+	hitbox.scale.y = 1.0
+	is_sliding = false
 
 func try_drop():
 	if drop_check.is_colliding():
@@ -139,12 +141,17 @@ func try_drop():
 func accel() -> float:
 	# For dash/slide, effectively behaves as deceleration
 	if absf(velocity.x) > SPEED:
+		print(velocity.x)
+		if is_sliding:
+			return 15
 		return 30
 	# For floatier movement when falling / jumping
 	elif not is_on_floor() or Input.is_action_just_pressed("jump"):
 		return 30
 	# Mostly for walking, I think
 	else:
+		if is_sliding:
+			stop_slide()
 		return 60
 
 func decel() -> float:
@@ -152,13 +159,17 @@ func decel() -> float:
 	if absf(velocity.x) > SPEED:
 		return 60
 	elif not is_on_floor() or Input.is_action_just_pressed("jump"):
-		return 0
+		return 30
 	else:
+		if is_sliding:
+			stop_slide()
 		return 90
 		
 func get_animation():
 	if Input.is_action_pressed("crouch") and drop_check.is_colliding():
 		return "crouching"
+	elif is_sliding:
+		return "sliding"
 	elif is_on_floor():
 		if absf(velocity.x) > 0.1:
 			return "running"
@@ -166,8 +177,6 @@ func get_animation():
 			return "idle"
 	elif is_dashing:
 		return "dashing"
-	elif is_sliding:
-		return "sliding"
 	elif is_wall_hanging:
 		return "wall"
 	else:
@@ -183,23 +192,17 @@ func _on_drop_timer_timeout():
 	platform.disabled = false
 	
 func _on_dash_timer_timeout():
-	hitbox.position.y = 0
-	hitbox.scale.y = 1.0
+	velocity.x = motion
 	is_dashing = false
-	is_sliding = false
 
-func _on_slide_cooldown_timeout():
-# Make the cooldown obvious to the player somehow, right now it feels weird
-	has_slide = true
-
-func _on_area_2d_left_body_entered(body):
+func _on_area_2d_left_body_entered(_body):
 	is_wall_hanging_left = true
 
-func _on_area_2d_left_body_exited(body):
+func _on_area_2d_left_body_exited(_body):
 	is_wall_hanging_left = false
 
-func _on_area_2d_right_body_entered(body):
+func _on_area_2d_right_body_entered(_body):
 	is_wall_hanging_right = true
 
-func _on_area_2d_right_body_exited(body):
+func _on_area_2d_right_body_exited(_body):
 	is_wall_hanging_right = false
