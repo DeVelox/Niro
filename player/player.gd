ad8582cc013c403982f1865f47fd3d4e665b465f
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-17 22:30:11 +0100

    Added temporary assets

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 const DASH_LENGTH = 0.2
 const DASH_MULTI = 2
+const SLIDE_LENGTH = 0.2
+const SLIDE_MULTI = 2
 const WALL_JUMP_MULTI = 2
 
 @onready var dash_timer = $DashTimer
@@ -11,6 +13,9 @@ const WALL_JUMP_MULTI = 2
 @onready var drop_timer = $DropTimer
 @onready var coyote_timer = $CoyoteTimer
 @onready var slide_cooldown = $SlideCooldown
+@onready var sprite = $AnimatedSprite2D
+@onready var double_tap = $DoubleTap
+@onready var hitbox = $CollisionShape2D
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -21,6 +26,7 @@ var is_wall_hanging_right = false
 var has_double_jump = false
 var is_dashing = false
 var has_dash = false
+var is_sliding = false
 var has_slide = true
 var platform
 var motion
@@ -59,14 +65,29 @@ func _physics_process(delta):
 		try_dash()
 	
 	if Input.is_action_just_pressed("crouch"):
-		try_drop()
+		if double_tap.is_stopped():
+			double_tap.start()
+		else:
+			double_tap.stop()
+			try_drop()
 
 	# Apply acceleration
 	if motion:
 		velocity.x = move_toward(velocity.x, motion, accel())
 	else:
 		velocity.x = move_toward(velocity.x, 0, decel())
+		
+	# Sprite direction
+	if not is_zero_approx(velocity.x):
+		if velocity.x > 0.0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
 
+	# Current animation
+	sprite.play(get_animation())
+
+	# Coyote timer
 	if is_on_floor():
 		was_on_floor = true
 
@@ -91,20 +112,21 @@ func try_jump():
 		return
 
 func try_dash():
-	if has_slide and is_on_floor():
-	# Make sure the dashes are always a consistent speed / length
-		velocity.x = motion * DASH_MULTI
-		scale.x = 0.3
-		scale.y = 0.15
+	if has_slide and is_on_floor():	
+		dash_timer.wait_time = SLIDE_LENGTH
+		velocity.x = motion * SLIDE_MULTI
+		hitbox.position.y = 32
+		hitbox.scale.y = 0.3
 		slide_cooldown.start()
+		is_sliding = true
 		has_slide = false
 	elif has_dash and not is_on_floor():
+		dash_timer.wait_time = DASH_LENGTH
 		velocity.x = motion * DASH_MULTI
 		is_dashing = true
 		has_dash = false
 	else:
 		return
-	dash_timer.wait_time = DASH_LENGTH
 	dash_timer.start()
 	velocity.y = 0
 
@@ -134,7 +156,26 @@ func decel() -> float:
 	else:
 		return 90
 		
-	
+func get_animation():
+	if Input.is_action_pressed("crouch") and drop_check.is_colliding():
+		return "crouching"
+	elif is_on_floor():
+		if absf(velocity.x) > 0.1:
+			return "running"
+		else:
+			return "idle"
+	elif is_dashing:
+		return "dashing"
+	elif is_sliding:
+		return "sliding"
+	elif is_wall_hanging:
+		return "wall"
+	else:
+		if velocity.y > 0.0:
+			return "falling"
+		else:
+			return "jumping"
+
 func reload():
 	get_tree().reload_current_scene()
 
@@ -142,9 +183,10 @@ func _on_drop_timer_timeout():
 	platform.disabled = false
 	
 func _on_dash_timer_timeout():
-	scale.x = 0.15
-	scale.y = 0.3
+	hitbox.position.y = 0
+	hitbox.scale.y = 1.0
 	is_dashing = false
+	is_sliding = false
 
 func _on_slide_cooldown_timeout():
 # Make the cooldown obvious to the player somehow, right now it feels weird
