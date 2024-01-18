caa3377f3ea827f60af7efd5d70c154d9d1e3e3b
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-18 23:27:30 +0100

    Snap timer

--- levels/nextlevel.gd
+++ levels/nextlevel.gd
@@ -1,13 +1,19 @@
 extends Node2D
+var i_dont_feel_so_good = false
+var snap_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if snap_time > 0:
+		modulate.a = snap_time
+		snap_time -= delta
+	elif i_dont_feel_so_good:
+		queue_free()
 	
-func destroy():
-	queue_free()
+func destroy(delay = 1.0):
+	i_dont_feel_so_good = true
+	snap_time = delay
