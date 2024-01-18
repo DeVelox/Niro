26df9855e15edd44173f64fa756f135b144af823
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-18 23:50:40 +0100

    Small fixes

--- Trophy.gd
+++ Trophy.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
-@export_file("*.tscn") var next_level
+@export_file("*.tscn") var next_scene
 	
 func interact():
-	var next_scene = load(next_level).instantiate()
-	get_node("/root/Main").add_child(next_scene)
+	var next_level = load(next_scene).instantiate()
+	get_node("/root/Main").add_child(next_level)
 	get_parent().destroy()
