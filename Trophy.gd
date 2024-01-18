6f1b6a5ffc4eabcc6ef7a4d0ad2fd7b7aa8254ee
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-18 23:38:33 +0100

    Fixed collision mask

--- Trophy.gd
+++ Trophy.gd
@@ -1,14 +1,5 @@
 extends StaticBody2D
 @export_file("*.tscn") var next_level
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 func interact():
 	var next_scene = load(next_level).instantiate()
