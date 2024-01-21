1b152ea959a5b88b142a4613f1753705d23d4d8f
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-21 16:21:25 +0100

    Dynamic spike asset

--- templates/spikes.gd
+++ templates/spikes.gd
@@ -1,5 +1,28 @@
 extends StaticBody2D
+@export var sprite : AtlasTexture
 
+@export_category("Collision shape")
+@export var margin := Vector2(1,1)
+@export_enum ("center", "top", "bottom", "left", "right") var align : String = "center"
+
+@onready var sprite_2d = $Sprite2D
+@onready var collision = $CollisionShape2D
+@onready var hitbox = $Area2D/CollisionShape2D
+
+func _ready():
+	sprite_2d.texture = sprite
+	var size = sprite_2d.texture.get_size()
+	collision.shape.size = size - margin
+	match align:
+		"top":
+			collision.position.y = -margin.y / 2
+		"bottom":
+			collision.position.y = margin.y / 2
+		"left":
+			collision.position.x = -margin.x / 2
+		"right":
+			collision.position.x = margin.x / 2
+	hitbox.shape.size = size
 
 func _on_area_2d_body_entered(body):
 	body.try_recall()
