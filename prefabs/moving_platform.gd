631b4b051090d7977abf578a362c4b6ca6d206c3
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-24 06:11:41 +0100

    Added moving platform

---
+++ prefabs/moving_platform.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@export var direction: Vector2
+@export var speed: float
+@export var tilemap: TileMap
+
+@onready var path: Path2D = $Path2D
+@onready var follow: PathFollow2D = $Path2D/PathFollow2D
+@onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var remote: RemoteTransform2D = $Path2D/PathFollow2D/RemoteTransform2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if direction:
+		path.curve.set_point_position(1, direction)
+	if speed:
+		animation.speed_scale = speed
+	if tilemap:
+		path.add_child(tilemap.duplicate())
+		remote.remote_path = path.get_child(1).get_path()
+		tilemap.queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
