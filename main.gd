78556e30fd8018cb195ae667688e19c9fb507da9
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-25 23:25:17 +0100

    Reworked debug reload logic

--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node
 @export_file("*.tscn") var current_scene: String
 @onready var spawn: Marker2D = $Spawn
+@onready var hearts: CheckBox = $CanvasLayer/Panel/VFlowContainer/Hearts
+@onready var vision: CheckBox = $CanvasLayer/Panel/VFlowContainer/Vision
+@onready var recall: CheckBox = $CanvasLayer/Panel/VFlowContainer/Recall
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +20,8 @@ func _ready():
 		load_player.position = spawn.position
 	add_child(load_player)
 
+	_init_checkbox()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -28,6 +33,7 @@ func _on_hearts_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		Upgrades.buy(Upgrades.Type.HEARTS)
 		Upgrades.add_heart()
+		print_debug(Upgrades.hearts)
 	else:
 		Upgrades.sell(Upgrades.Type.HEARTS)
 	_reload()
@@ -49,9 +55,11 @@ func _on_recall_toggled(toggled_on: bool) -> void:
 	_reload()
 
 
+func _init_checkbox():
+	hearts.set_pressed_no_signal(Upgrades.check(Upgrades.Type.HEARTS))
+	vision.set_pressed_no_signal(Upgrades.check(Upgrades.Type.VISION))
+	recall.set_pressed_no_signal(Upgrades.check(Upgrades.Type.RECALL))
+
+
 func _reload():
-	DataStore.current_level.queue_free()
-	var load_level: Node2D = load(current_scene).instantiate()
-	DataStore.current_level = load_level
-	DataStore.current_scene = current_scene
-	add_child(load_level)
+	get_tree().reload_current_scene()
