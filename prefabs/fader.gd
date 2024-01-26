8a2a73befcaff07f6ac2034964ad508e760b3123
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-27 00:29:31 +0100

    Added fading between tilemaps

---
+++ prefabs/fader.gd
@@ -0,0 +1,29 @@
+extends Area2D
+@export var fade_in: TileMap
+@export var fade_out: TileMap
+
+
+func _ready() -> void:
+	fade_in.modulate = Color(1, 1, 1, 0)
+	for i in fade_in.get_layers_count():
+		fade_in.set_layer_enabled(i, false)
+
+
+func _fade_in() -> void:
+	var tween = create_tween()
+	tween.tween_property(fade_in, "modulate", Color(1, 1, 1, 1), 1)
+	for i in fade_in.get_layers_count():
+		tween.tween_callback(fade_in.set_layer_enabled.bind(i, true))
+
+
+func _fade_out() -> void:
+	var tween = create_tween()
+	tween.tween_property(fade_out, "modulate", Color(1, 1, 1, 0), 1)
+	for i in fade_out.get_layers_count():
+		tween.tween_callback(fade_out.set_layer_enabled.bind(i, false))
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("players"):
+		_fade_in()
+		_fade_out()
