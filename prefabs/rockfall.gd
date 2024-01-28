dcf169d5f2c12af321ff1bbc0b2f561e6234039e
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-29 00:21:16 +0100

    Restructured folder hierarchy and deleted unused files

--- prefabs/rockfall.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(_body: Node) -> void:
-	var rock: Rock = load("res://prefabs/rock.tscn").instantiate()
-	rock.position.y -= 100
-	call_deferred("add_child", rock)
---
+++ scripts/singleton/dialog_manager.gd
@@ -0,0 +1,58 @@
+extends Node
+
+signal next_line
+
+var dialog_lines: Array[String] = []
+var current_line := 0
+
+var text_box: TextBox
+var text_box_position: Vector2
+
+var is_dialog_active := false
+var can_advance := false
+
+@onready var text_box_scene = preload("res://prefabs/utility library/text_box.tscn")
+
+
+func _ready() -> void:
+	Dialog.next_line.connect(_on_dialog_next_line)
+
+
+func start_dialog(position: Vector2, lines: Array[String]) -> void:
+	if is_dialog_active:
+		next_line.emit()
+		return
+
+	dialog_lines = lines
+	text_box_position = position
+	is_dialog_active = true
+	_show_text_box()
+
+
+func end_dialog() -> void:
+	if is_dialog_active:
+		current_line = 0
+		is_dialog_active = false
+
+
+func _show_text_box() -> void:
+	text_box = text_box_scene.instantiate() as TextBox
+	text_box.finished.connect(_on_text_box_finished)
+	get_tree().root.add_child(text_box)
+	text_box.global_position = text_box_position
+	text_box.display_text(dialog_lines[current_line])
+	can_advance = false
+
+
+func _on_text_box_finished() -> void:
+	can_advance = true
+	current_line += 1
+	if current_line >= dialog_lines.size():
+		is_dialog_active = false
+		current_line = 0
+		return
+
+
+func _on_dialog_next_line() -> void:
+	if is_dialog_active and can_advance:
+		_show_text_box()
