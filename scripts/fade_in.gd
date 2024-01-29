d79f32c70e16a172bff5551644e176a7b8d2360b
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-29 17:30:51 +0100

    Fallback when there is no animation

--- scripts/fade_in.gd
+++ scripts/fade_in.gd
@@ -3,8 +3,12 @@ extends Node2D
 
 func fade_in(tilemap: TileMap) -> void:
 	Scene.toggle_layers(tilemap, true)
-	var pos: Vector2 = tilemap.local_to_map(position)
+	var pos: Vector2i = tilemap.local_to_map(position)
 	var tile: Vector2i = tilemap.get_cell_atlas_coords(1, pos)
-	tilemap.set_cell(1, pos, 3, Scene.get_anim(tile, "in"))
+	var anim: Vector2i = Scene.get_anim(tile, "in")
+	if anim != tile:
+		tilemap.set_cell(1, pos, 3, anim)
+	else:
+		tilemap.set_cell(1, pos, 2, tile)
 	await get_tree().create_timer(1).timeout
 	tilemap.set_cell(1, pos, 2, tile)
--- scripts/fade_out.gd
+++ scripts/fade_out.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 
 func fade_out(tilemap: TileMap) -> void:
-	var pos: Vector2 = tilemap.local_to_map(position)
+	var pos: Vector2i = tilemap.local_to_map(position)
 	var tile: Vector2i = tilemap.get_cell_atlas_coords(1, pos)
-	tilemap.set_cell(1, pos, 4, Scene.get_anim(tile, "out"))
+	var anim: Vector2i = Scene.get_anim(tile, "out")
+	if anim != tile:
+		tilemap.set_cell(1, pos, 4, anim)
+	else:
+		tilemap.set_cell(1, pos, 2, tile)
 	await get_tree().create_timer(1).timeout
 	tilemap.erase_cell(1, pos)
