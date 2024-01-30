2991377bf836199e59b8c68c0e74a96987b2f7d2
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-30 02:19:21 +0100

    Fading for hidden layer

--- scripts/fade_in.gd
+++ scripts/fade_in.gd
@@ -2,13 +2,22 @@ extends Node2D
 
 
 func fade_in(tilemap: TileMap) -> void:
+	var vision = Upgrades.check(Upgrades.Type.VISION)
 	Scene.toggle_layers(tilemap, true)
+	if not vision:
+		tilemap.set_layer_enabled(2, false)
+	if vision:
+		_fade(tilemap, 2)
+	_fade(tilemap, 1)
+
+
+func _fade(tilemap: TileMap, layer: int):
 	var pos: Vector2i = tilemap.local_to_map(position)
-	var tile: Vector2i = tilemap.get_cell_atlas_coords(1, pos)
+	var tile: Vector2i = tilemap.get_cell_atlas_coords(layer, pos)
 	var anim: Vector2i = Scene.get_anim(tile, "in")
 	if anim != tile:
-		tilemap.set_cell(1, pos, 3, anim)
+		tilemap.set_cell(layer, pos, 3, anim)
 	else:
-		tilemap.set_cell(1, pos, 2, tile)
+		tilemap.set_cell(layer, pos, 2, tile)
 	await get_tree().create_timer(1).timeout
-	tilemap.set_cell(1, pos, 2, tile)
+	tilemap.set_cell(layer, pos, 2, tile)
--- scripts/fade_out.gd
+++ scripts/fade_out.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 
 func fade_out(tilemap: TileMap) -> void:
+	var vision = Upgrades.check(Upgrades.Type.VISION)
+	if vision:
+		_fade(tilemap, 2)
+	_fade(tilemap, 1)
+
+
+func _fade(tilemap: TileMap, layer: int) -> void:
 	var pos: Vector2i = tilemap.local_to_map(position)
-	var tile: Vector2i = tilemap.get_cell_atlas_coords(1, pos)
+	var tile: Vector2i = tilemap.get_cell_atlas_coords(layer, pos)
 	var anim: Vector2i = Scene.get_anim(tile, "out")
 	if anim != tile:
-		tilemap.set_cell(1, pos, 4, anim)
+		tilemap.set_cell(layer, pos, 4, anim)
 	else:
-		tilemap.set_cell(1, pos, 2, tile)
+		tilemap.set_cell(layer, pos, 2, tile)
 	await get_tree().create_timer(1).timeout
-	tilemap.erase_cell(1, pos)
+	tilemap.erase_cell(layer, pos)
