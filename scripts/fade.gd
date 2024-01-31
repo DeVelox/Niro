4a1d2d302634254b65c9ed522ca98e1c6673cdff
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-01-31 14:51:43 +0100

    Placeholder animations with collision

--- scripts/fade.gd
+++ scripts/fade.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 var vision: bool = Upgrades.check(Upgrades.Type.VISION)
 
+
 func fade_in(tilemap: TileMap) -> void:
 	_fade(tilemap, true)
 
+
 func fade_out(tilemap: TileMap) -> void:
 	_fade(tilemap, false)
-	
+
+
 func _fade(tilemap: TileMap, fade: bool) -> void:
 	if vision:
 		Scene.fade_animation(tilemap, 2, position, fade, 6)
