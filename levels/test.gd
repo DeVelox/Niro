fd9809efa5f5cc4d0a73d40197ef718ddd159f86
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-02 02:21:38 +0100

    Set up a few test cases

--- levels/test.gd
+++ levels/test.gd
@@ -8,8 +8,18 @@ func _ready() -> void:
 	_set_checkpoint()
 	_check_vision()
 	_initialise()
-	await Scene.fade_out(tilemap)
-	await Scene.fade_in(tilemap)
+	
+	# Slowmo
+	Engine.time_scale = 0.1
+	
+	# Manual calls of the fade functions (doesn't really work / works weird)
+	#await Scene.fade_out(tilemap)
+	#await Scene.fade_in(tilemap)
+	
+	# Manual call of the animation function (works correctly both ways, but starts on a random frame)
+	Scene.toggle_layers(tilemap, false)
+	for i in tilemap.get_used_cells(1):
+		Scene.fade_animation.call_deferred(tilemap, 1, tilemap.map_to_local(i), Scene.Source.FADEOUT, Scene._add_temp_layer(tilemap))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
