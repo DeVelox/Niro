d089e90deb563afce9c97e0b95b298d1caf139a9
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-03 17:41:13 +0100

    Bullet go brr

--- scripts/bullet_spawner.gd
+++ scripts/bullet_spawner.gd
@@ -3,7 +3,13 @@ extends Node2D
 const BULLET = preload("res://prefabs/utility library/bullet.tscn")
 const BULLET_SPEED = 500
 
-@export var delay: float = 1
+@export var delay: float = 0.1
+
+var point: Vector2
+var i: int = -320
+var x: int = 0
+var y: int = 0
+var step: int = 1
 
 @onready var timer: Timer = $Timer
 @onready var bullet_spawner: Node2D = $"."
@@ -18,10 +24,15 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_timer_timeout() -> void:
+	if i > 320:
+		step = -1
+	if i < -320:
+		step = 1
+	i += step
+	x += 1
+	y += 1
 	var bullet: Bullet = BULLET.instantiate()
-	var player: Player = get_node("/root/Main/Player")
-	var direction := player.global_position - bullet_spawner.global_position
+	var direction := Vector2((x % 360) - 180, (y % 360) - 180)
 	bullet.direction = direction.normalized() * BULLET_SPEED
 	add_child(bullet)
