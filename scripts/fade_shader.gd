95581a637584494e4930a9fdc6b12b1e2f2ad34c
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-03 13:40:57 +0100

    Fixed recall animation

--- scripts/fade_shader.gd
+++ scripts/fade_shader.gd
@@ -7,6 +7,7 @@ const FADEOUT = preload("res://assets/elliot/TilesetV4_Animation_Reverse.png")
 var vision: bool = Upgrades.check(Upgrades.Type.VISION)
 var atlas_coords: Vector2i
 var atlas_coords_hidden: Vector2i
+var tween: Tween
 
 @onready var fade_shader: TextureRect = $FadeShader
 @onready var collision: CollisionShape2D = $CollisionShape2D
@@ -44,18 +45,20 @@ func stay(tilemap: TileMap) -> void:
 
 
 func _fade(_tilemap: TileMap, _atlas_texture: CompressedTexture2D, _atlas_coords: Vector2i) -> void:
-	fade_shader.material.set_shader_parameter("frame", 0)
-	fade_shader.material.set_shader_parameter("frames", 10)
-	fade_shader.material.set_shader_parameter("toggle", true)
 	fade_shader.material.set_shader_parameter("atlas", _atlas_texture)
 	fade_shader.material.set_shader_parameter("atlas_x", _atlas_coords.x)
 	fade_shader.material.set_shader_parameter("atlas_y", _atlas_coords.y)
+	fade_shader.material.set_shader_parameter("frame", 0)
+	fade_shader.material.set_shader_parameter("frames", 10)
+	fade_shader.material.set_shader_parameter("toggle", true)
 
 	if _atlas_texture == STATIC:
 		fade_shader.material.set_shader_parameter("frame", 0)
 		fade_shader.material.set_shader_parameter("frames", 1)
 	else:
-		var tween := create_tween()
+		if is_instance_valid(tween):
+			tween.kill()
+		tween = create_tween()
 		if _atlas_texture == FADEOUT:
 			tween.tween_property(collision, "disabled", true, 0.5)
 		tween.parallel().tween_property(fade_shader.material, "shader_parameter/frame", 9, 1.0)
