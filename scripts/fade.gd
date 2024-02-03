35ab701c2df2a9c467a96a87ee86ff75b7ae6943
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-03 15:31:05 +0100

    Added animation for flipped tiles

--- scripts/fade.gd
+++ scripts/fade.gd
@@ -24,30 +24,38 @@ func fade_in(tilemap: TileMap) -> void:
 	if tilemap != get_parent():
 		return
 	if vision:
-		_fade(tilemap, FADEIN, atlas_coords_hidden)
-	_fade(tilemap, FADEIN, atlas_coords)
+		var tile_hidden = tilemap.get_cell_tile_data(2, atlas_coords_hidden)
+		_fade(tilemap, FADEIN, atlas_coords_hidden, tile_hidden.flip_h, tile_hidden.flip_v)
+	var tile = tilemap.get_cell_tile_data(1, atlas_coords)
+	_fade(tilemap, FADEIN, atlas_coords, tile.flip_h, tile.flip_v)
 
 
 func fade_out(tilemap: TileMap) -> void:
 	if tilemap != get_parent():
 		return
 	if vision:
-		_fade(tilemap, FADEOUT, atlas_coords_hidden)
-	_fade(tilemap, FADEOUT, atlas_coords)
+		var tile_hidden = tilemap.get_cell_tile_data(2, atlas_coords_hidden)
+		_fade(tilemap, FADEOUT, atlas_coords_hidden, tile_hidden.flip_h, tile_hidden.flip_v)
+	var tile = tilemap.get_cell_tile_data(1, atlas_coords)
+	_fade(tilemap, FADEOUT, atlas_coords, tile.flip_h, tile.flip_v)
 
 
 func stay(tilemap: TileMap) -> void:
 	if tilemap != get_parent():
 		return
 	if vision:
-		_fade(tilemap, STATIC, atlas_coords_hidden)
-	_fade(tilemap, STATIC, atlas_coords)
+		var tile_hidden = tilemap.get_cell_tile_data(2, atlas_coords_hidden)
+		_fade(tilemap, STATIC, atlas_coords_hidden, tile_hidden.flip_h, tile_hidden.flip_v)
+	var tile = tilemap.get_cell_tile_data(1, atlas_coords)
+	_fade(tilemap, STATIC, atlas_coords, tile.flip_h, tile.flip_v)
 
 
-func _fade(_tilemap: TileMap, _atlas_texture: CompressedTexture2D, _atlas_coords: Vector2i) -> void:
+func _fade(_tilemap: TileMap, _atlas_texture: CompressedTexture2D, _atlas_coords: Vector2i, _flip_x: bool, _flip_y: bool) -> void:
 	fade_shader.material.set_shader_parameter("atlas", _atlas_texture)
 	fade_shader.material.set_shader_parameter("atlas_x", _atlas_coords.x)
 	fade_shader.material.set_shader_parameter("atlas_y", _atlas_coords.y)
+	fade_shader.material.set_shader_parameter("flip_x", _flip_x)
+	fade_shader.material.set_shader_parameter("flip_y", _flip_y)
 	fade_shader.material.set_shader_parameter("frame", 0)
 	fade_shader.material.set_shader_parameter("frames", 10)
 	fade_shader.material.set_shader_parameter("toggle", true)
