d8ed8b18a81b939083963bceb8829e9b2b0eafd6
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-03 18:01:04 +0100

    Bullets

--- scripts/bullet_spawner.gd
+++ scripts/bullet_spawner.gd
@@ -6,10 +6,8 @@ const BULLET_SPEED = 500
 @export var delay: float = 0.1
 
 var point: Vector2
-var i: int = -320
-var x: int = 0
-var y: int = 0
-var step: int = 1
+var i: int = -360
+var step: int = 120
 
 @onready var timer: Timer = $Timer
 @onready var bullet_spawner: Node2D = $"."
@@ -25,14 +23,13 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_timer_timeout() -> void:
-	if i > 320:
-		step = -1
-	if i < -320:
-		step = 1
+	if i < -360:
+		step = -step
+	if i > 360:
+		step = -step
 	i += step
-	x += 1
-	y += 1
+	print_debug(i, 0)
 	var bullet: Bullet = BULLET.instantiate()
-	var direction := Vector2((x % 360) - 180, (y % 360) - 180)
-	bullet.direction = direction.normalized() * BULLET_SPEED
+	var direction := Vector2(i, 0)
+	bullet.direction = direction
 	add_child(bullet)
