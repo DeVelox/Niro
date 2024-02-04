2f5409c5dac397988d6adaef9f038118615c4cb6
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-04 01:46:25 +0100

    Exportified the bullet spawner

--- scripts/bullet_spawner.gd
+++ scripts/bullet_spawner.gd
@@ -5,17 +5,15 @@ const BULLET_SPEED = 400
 const STARTING_ANGLE = 0
 
 @export var delay: float = 1.5
-
-var point: Vector2
-var angle: float = STARTING_ANGLE
-var bullet_count: int = 4
-var groups: int = 3
-var speed_variant_array: Array[int] = [0,80,50,-20]
-var shape: String = "half_circle"
-var barrage_wait_time: float = 0.15
-var mode: String = "shotgun"
-var bullet_spread: int = 5
-
+@export var point: Vector2
+@export var angle: float = STARTING_ANGLE
+@export var bullet_count: int = 4
+@export var groups: int = 3
+@export var speed_variant_array: Array[int] = [0, 80, 50, -20]
+@export_enum("half_circle", "circle") var shape: String = "half_circle"
+@export_enum("shotgun", "single", "barrage") var mode: String = "shotgun"
+@export var barrage_wait_time: float = 0.15
+@export var bullet_spread: int = 5
 
 @onready var timer: Timer = $Timer
 @onready var bullet_spawner: Node2D = $"."
@@ -31,6 +29,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
+
 func _shoot_barrage(shot_angle: float, barrage_count: int) -> void:
 	for count in barrage_count:
 		var bullet: Bullet = BULLET.instantiate()
@@ -40,22 +39,24 @@ func _shoot_barrage(shot_angle: float, barrage_count: int) -> void:
 		shot_angle = _get_next_step(shot_angle)
 		await get_tree().create_timer(barrage_wait_time).timeout
 
+
 func _shoot_single(shot_angle: float) -> void:
-		var bullet: Bullet = BULLET.instantiate()
-		var direction := _get_direction(shot_angle)
-		bullet.direction = direction * BULLET_SPEED
-		add_child(bullet)
+	var bullet: Bullet = BULLET.instantiate()
+	var direction := _get_direction(shot_angle)
+	bullet.direction = direction * BULLET_SPEED
+	add_child(bullet)
+
 
 func _shoot_shotgun(shot_angle: float, pellet_count: int, speed_variant_array: Array[int]) -> void:
-	var angle_diff: float = 2 * PI/(bullet_spread * (groups - 1))
-	shot_angle -= angle_diff * pellet_count/2
+	var angle_diff: float = 2 * PI / (bullet_spread * (groups - 1))
+	shot_angle -= angle_diff * pellet_count / 2
 	for count in pellet_count:
 		var direction := _get_direction(shot_angle)
 		var bullet: Bullet = BULLET.instantiate()
 		bullet.direction = direction * (BULLET_SPEED - speed_variant_array[count])
 		add_child(bullet)
 		shot_angle += angle_diff
-		
+
 
 func _get_direction(current_angle: float) -> Vector2:
 	var x: float = cos(current_angle)
@@ -68,19 +69,19 @@ func _get_direction(current_angle: float) -> Vector2:
 		y = sin(current_angle)
 	return Vector2(x, y)
 
+
 func _get_next_step(curren_angle: float) -> float:
 	var new_angle: float = curren_angle
-	new_angle += 2 * PI/(bullet_spread * (groups - 1))
+	new_angle += 2 * PI / (bullet_spread * (groups - 1))
 	return new_angle
+
 
 func _on_timer_timeout() -> void:
 	target.position = _get_direction(angle) * 100
 	if mode == "barrage":
 		_shoot_barrage(angle, bullet_count)
 	elif mode == "shotgun":
-		_shoot_shotgun(angle,bullet_count,speed_variant_array)
+		_shoot_shotgun(angle, bullet_count, speed_variant_array)
 	elif mode == "single":
 		_shoot_single(angle)
-	angle += 2 * PI/(groups+1)
-	
-	
+	angle += 2 * PI / (groups + 1)
--- scripts/singleton/sound_manager.gd
+++ scripts/singleton/sound_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 # Music
 const TRACK1 = preload("res://assets/music/more options/344252__suchthefool88__psychic-cm.ogg")
-const TRACK2 = preload("res://assets/music/more options/488362__soundflakes__dark-dramatic-atmospheric-soundscape.ogg")
-const TRACK3 = preload("res://assets/music/more options/524309__bertsz__dark-cyberpunk-orchestral-music.ogg")
+const TRACK2 = preload(
+	"res://assets/music/more options/488362__soundflakes__dark-dramatic-atmospheric-soundscape.ogg"
+)
+const TRACK3 = preload(
+	"res://assets/music/more options/524309__bertsz__dark-cyberpunk-orchestral-music.ogg"
+)
 const TRACK4 = preload("res://assets/music/more options/525290__disquantic__cyberpunk-beat.ogg")
-const TRACK5 = preload("res://assets/music/more options/611305__szegvari__new-york-cyberpunk-synth-analogue-drums-bass-dance-retro-atmo-ambience-pad-drone-cinematic-action-music-surround.ogg")
+const TRACK5 = preload(
+	"res://assets/music/more options/611305__szegvari__new-york-cyberpunk-synth-analogue-drums-bass-dance-retro-atmo-ambience-pad-drone-cinematic-action-music-surround.ogg"
+)
 const TRACK6 = preload("res://assets/music/more options/672783__bertsz__cyberpunk_metal.ogg")
 
 # SFX
@@ -14,7 +20,7 @@ const GIANT = preload("res://assets/sfx/El Gigante.ogg")
 
 var current_track: AudioStreamPlayer
 
-	
+
 func sfx(sound: AudioStream, pitch: float = 1) -> void:
 	var audio: AudioStreamPlayer = AudioStreamPlayer.new()
 	audio.bus = "Sounds"
@@ -24,7 +30,8 @@ func sfx(sound: AudioStream, pitch: float = 1) -> void:
 	get_tree().current_scene.add_child(audio)
 	audio.finished.connect(audio.queue_free)
 	audio.play()
-	
+
+
 func event(sound: AudioStream) -> void:
 	var audio: AudioStreamPlayer = AudioStreamPlayer.new()
 	audio.bus = "Events"
@@ -33,7 +40,8 @@ func event(sound: AudioStream) -> void:
 	get_tree().current_scene.add_child(audio)
 	audio.finished.connect(audio.queue_free)
 	audio.play()
-	
+
+
 func music(track: AudioStream, loop: bool = true) -> AudioStreamPlayer:
 	var audio: AudioStreamPlayer = AudioStreamPlayer.new()
 	audio.bus = "Music"
@@ -43,7 +51,8 @@ func music(track: AudioStream, loop: bool = true) -> AudioStreamPlayer:
 	audio.finished.connect(audio.queue_free)
 	audio.play()
 	return audio
-	
+
+
 func crossfade(old_track: AudioStreamPlayer, new_track: AudioStream) -> void:
 	var audio = music(new_track)
 	var tween := create_tween()
