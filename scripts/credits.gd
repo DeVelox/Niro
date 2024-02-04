fb0eeb3f36ce7096f5736f2263a59f33d8a87b55
DeVelox <1922928+DeVelox@users.noreply.github.com>
2024-02-04 16:13:58 +0100

    Added intro and credits

---
+++ scripts/credits.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var timer: float = 0
+
+@onready var point_light_2d: PointLight2D = $PointLight2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer += delta
+	if timer > 4 * delta:
+		point_light_2d.energy = clamp(randfn(0.8, 0.1), 0.0, 1.0)
+		timer = 0
---
+++ scripts/intro.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+signal end_cutscene
+
+@onready var rich_text_label: RichTextLabel = $MarginContainer/RichTextLabel
+@onready var letter_timer: Timer = $LetterTimer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	end_cutscene.connect(_end_cutscene, CONNECT_ONE_SHOT)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		end_cutscene.emit()
+
+
+func _end_cutscene() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_letter_timer_timeout() -> void:
+	var clean_text: String = _strip_bbcode(rich_text_label.text)
+	var hero_name: String = "THE UNNAMED HERO"
+	var game_name: String = "THE UNNAMED GAME"
+	var hero_name_i: int = clean_text.find(hero_name)
+	var game_name_i: int = clean_text.find(game_name)
+	rich_text_label.visible_characters += 1
+	if rich_text_label.visible_characters == hero_name_i:
+		rich_text_label.visible_characters += hero_name.length()
+		_pause(1)
+	if rich_text_label.visible_characters == game_name_i:
+		rich_text_label.visible_characters += game_name.length()
+		_pause(2)
+	if rich_text_label.visible_characters > clean_text.length():
+		_pause(1)
+		end_cutscene.emit()
+
+
+func _strip_bbcode(text: String) -> String:
+	var regex = RegEx.new()
+	regex.compile("\\[.*?\\]")
+	return regex.sub(text, "", true)
+
+
+func _pause(duration: int) -> void:
+	letter_timer.paused = true
+	await get_tree().create_timer(duration).timeout
+	letter_timer.paused = false
